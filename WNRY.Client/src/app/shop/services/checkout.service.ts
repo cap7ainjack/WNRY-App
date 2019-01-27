5ba7313e636d6d72ef4a4ab36ccb52cd507504ae
Boyann <botsan@abv.bg>
2019-01-27 19:10:38 +0200

    shopping-cart small fix

--- WNRY.Client/src/app/shop/services/checkout.service.ts
+++ WNRY.Client/src/app/shop/services/checkout.service.ts
@@ -14,31 +14,35 @@ import { CartContent } from '../models/cart-content';
 
 export class CheckoutService extends BaseService {
 
-    baseUrl = '';
+		baseUrl = '';
 
-    constructor(private http: HttpClient, private configService: ConfigService) {
-        super();
-        this.baseUrl = configService.getApiURI();
-    }
+		constructor(private http: HttpClient, private configService: ConfigService) {
+				super();
+				this.baseUrl = configService.getApiURI();
+		}
 
-    loadCart(): CartContent {
-        const cartContent: CartContent = { items: [], total: 0 };
-        cartContent.total = 0;
-        cartContent.items = [];
+		public getRegions(): Observable<any> {
+		return this.http.get('');
+	}
 
-        let cart = JSON.parse(localStorage.getItem('cart'));
+		loadCart(): CartContent {
+				const cartContent: CartContent = { items: [], total: 0 };
+				cartContent.total = 0;
+				cartContent.items = [];
 
-        if (cart) {
-            for (let i = 0; i < cart.length; i++) {
-                let item = JSON.parse(cart[i]);
-                cartContent.items.push({
-                    product: item.product,
-                    quantity: item.quantity
-                });
-                cartContent.total += item.product.price * item.quantity;
-            }
-        }
+				let cart = JSON.parse(localStorage.getItem('cart'));
 
-        return cartContent;
-    }
+				if (cart) {
+						for (let i = 0; i < cart.length; i++) {
+								let item = JSON.parse(cart[i]);
+								cartContent.items.push({
+										product: item.product,
+										quantity: item.quantity
+								});
+								cartContent.total += item.product.price * item.quantity;
+						}
+				}
+
+				return cartContent;
+		}
 }
--- WNRY.Client/src/app/shop/shopping-cart/shopping-cart.component.ts
+++ WNRY.Client/src/app/shop/shopping-cart/shopping-cart.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { ProductData } from '../models/product-data';
 import { CartItem } from '../models/cart-item';
 // import { ProductService } from '../../services/product.service';
 
